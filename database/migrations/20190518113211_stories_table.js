ae6044bc7a3caa395f0092fb2289fd979973774b
AmarachiOE <ao.ejiawoko@gmail.com>
2019-05-18 11:35:52 -0700

    Migration for stories table

---
+++ database/migrations/20190518113211_stories_table.js
@@ -0,0 +1,28 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("stories", tbl => {
+      // story id 
+      tbl.increments();
+  
+      // foreign key user id
+      tbl
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+  
+      // other fields
+      tbl.string("title").notNullable();
+      tbl.string("country").notNullable();
+      tbl.string("description").notNullable();
+      tbl.string("fullStory").notNullable();
+      tbl.string("date").notNullable();
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+      return knex.schema.dropTableIfExists("stories");
+  };
+  
